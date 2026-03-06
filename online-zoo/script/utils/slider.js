c843a7e79a4b581e7da5140deb14c55a9619219b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-06 21:00:22 +0400

    feat: stop scroll at the end

--- online-zoo/script/utils/slider.js
+++ online-zoo/script/utils/slider.js
@@ -2,16 +2,25 @@ export const slider = () => {
   const left_arrow = document.getElementById("sldr_left_arr");
   const right_arrow = document.getElementById("sldr_right_arr");
   const slider = document.getElementById("slider");
+  const rect = slider.getBoundingClientRect();
+  const xCoordinate = rect.left;
+  console.log("x: ", xCoordinate);
 
   let counter = 0;
   const step_width = slider.offsetWidth / 4;
   left_arrow.addEventListener("click", () => {
+    if (counter > -1) {
+      return;
+    }
     counter++;
     console.log(counter);
     console.log("clicked on left arrow");
     slider.style.transform = `translate(${counter * step_width}px)`;
   });
   right_arrow.addEventListener("click", () => {
+    if (counter < 0) {
+      return;
+    }
     counter--;
     console.log(counter);
     console.log("clicked on right arrow");
