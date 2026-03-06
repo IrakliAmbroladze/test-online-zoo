326635bcee5e0f7a9859b3a5508c0dc68185323b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-06 20:33:52 +0400

    feat: make slider functional on arrow clicks

--- online-zoo/script/utils/slider.js
+++ online-zoo/script/utils/slider.js
@@ -2,12 +2,19 @@ export const slider = () => {
   const left_arrow = document.getElementById("sldr_left_arr");
   const right_arrow = document.getElementById("sldr_right_arr");
   const slider = document.getElementById("slider");
+
+  let counter = 0;
+  const step_width = 50;
   left_arrow.addEventListener("click", () => {
+    counter--;
+    console.log(counter);
     console.log("clicked on left arrow");
-    slider.style.transform = "translate(-50px)";
+    slider.style.transform = `translate(${counter * step_width}px)`;
   });
   right_arrow.addEventListener("click", () => {
+    counter++;
+    console.log(counter);
     console.log("clicked on right arrow");
-    slider.style.transform = "translate(50px)";
+    slider.style.transform = `translate(${counter * step_width}px)`;
   });
 };
