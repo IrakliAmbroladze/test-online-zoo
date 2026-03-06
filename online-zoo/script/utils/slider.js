a1b31eb588264418f8482c4add72f88bf30daa7e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-06 20:12:30 +0400

    feat: get arrows in JS

--- online-zoo/script/utils/slider.js
+++ online-zoo/script/utils/slider.js
@@ -1,3 +1,10 @@
 export const slider = () => {
-  console.log("This is a slider function");
+  const left_arrow = document.getElementById("sldr_left_arr");
+  const right_arrow = document.getElementById("sldr_right_arr");
+  left_arrow.addEventListener("click", () => {
+    console.log("clicked on left arrow");
+  });
+  right_arrow.addEventListener("click", () => {
+    console.log("clicked on right arrow");
+  });
 };
