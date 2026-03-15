87cfae92271f27e5103246b05c4514f523dc9783
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-15 05:51:06 +0400

    feat(MeetPes): render slider cards for pets dynamically

--- online-zoo/src/components/MeetPets.ts
+++ online-zoo/src/components/MeetPets.ts
@@ -1,7 +1,9 @@
 import { fetchPets } from "../lib/fetchPets";
+import type { Pet } from "../types/Pet";
+import { PETS } from "../types/PETS";
 
 export const MeetPets = async (): Promise<void> => {
-  const container = document.getElementById("pets-container");
+  const container = document.getElementById("slider");
   if (!container) return;
 
   container.innerHTML = `
@@ -11,14 +13,34 @@ export const MeetPets = async (): Promise<void> => {
   `;
 
   try {
-    const pets = await fetchPets();
-    console.log("pets are:", pets);
+    const pets: Pet[] = await fetchPets();
+    console.log(pets);
 
-    container.innerHTML = `
-      <div class="pets-slider">
-        Pets loaded: ${pets.length}
+    container.innerHTML = "";
+
+    pets.forEach((pet) => {
+      const animal =
+        Object.values(PETS).find((petName) =>
+          pet.commonName.toLowerCase().includes(petName),
+        ) || "koala";
+
+      const element = `
+    <div class="animals-card">
+      <label>${pet.commonName}</label>
+      <div class="cover">
+        <img src="./assets/images/${animal}.png" alt="${animal}" />
       </div>
-    `;
+      <div class="title">${pet.commonName}</div>
+      <p>${pet.description}</p>
+      <button class="btn btn--pure-text-orange">
+        <span>view live cam</span>
+        <img src="./assets/icons/arrow.svg" alt="arrow" />
+      </button>
+    </div>
+  `;
+
+      container.insertAdjacentHTML("beforeend", element);
+    });
   } catch (err) {
     if (err instanceof Error) {
       console.error("Caught an Error object:", err.message);
