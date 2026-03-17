92f8e8aa6958d46ef0b0bc078a4f9f1c6dde98c4
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-17 22:51:15 +0400

    feat(Cameras): add loader to thee zoos page while fetching cameras data

--- online-zoo/src/components/Zoos.ts
+++ online-zoo/src/components/Zoos.ts
@@ -2,11 +2,31 @@ import { fetchCameras } from "../lib/fetchCameras";
 import type { Camera } from "../types/Camera";
 
 export default async function Zoos() {
+  const container = document.getElementById("cameras-container");
+  if (!container) return;
+
+  container.innerHTML = `
+    <div class="loader">
+      Loading cameras...
+    </div>
+  `;
   try {
     const cameras: Camera[] = await fetchCameras();
+    container.innerHTML = "";
+
     console.log(cameras);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error("Caught an Error object:", err.message);
+    } else {
+      console.error("Caught an unknown error type:", err);
+    }
+
+    container.innerHTML = `
+      <div class="error">
+        <p>Something went wrong. Please, refresh the page</p>
+      </div>
+    `;
   }
   console.log("start creating zoos page");
 }
