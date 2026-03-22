4f9e608d660390d9dcec111f9bbc3d6e6150430e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-22 21:06:46 +0400

    chore(Map): transform map html into a react component

--- online-zoo/src/components/Map.tsx
+++ online-zoo/src/components/Map.tsx
@@ -1,3 +1,58 @@
 export const Map = () => {
-  return <div>This is a map page</div>;
+  return (
+    <div className="page-map">
+      <main className="main">
+        <div className="container">
+          <h1>find where are the animals live</h1>
+          <div className="map">
+            <img src="../assets/images/map.svg" alt="map" />
+
+            <img
+              src="../assets/icons/animal-loc-1.png"
+              alt="animal-loc-1"
+              className="animal-loc-1"
+            />
+            <img
+              src="../assets/icons/animal-loc-2.png"
+              alt="animal-loc-2"
+              className="animal-loc-2"
+            />
+            <img
+              src="../assets/icons/animal-loc-3.png"
+              alt="animal-loc-3"
+              className="animal-loc-3"
+            />
+            <img
+              src="../assets/icons/animal-loc-4.png"
+              alt="animal-loc-4"
+              className="animal-loc-4"
+            />
+            <img
+              src="../assets/icons/animal-loc-5.png"
+              alt="animal-loc-5"
+              className="animal-loc-5"
+            />
+            <img
+              src="../assets/icons/animal-loc-6.png"
+              alt="animal-loc-6"
+              className="animal-loc-6"
+            />
+            <img
+              src="../assets/icons/animal-loc-7.png"
+              alt="animal-loc-7"
+              className="animal-loc-7"
+            />
+            <img
+              src="../assets/icons/animal-loc-8.png"
+              alt="animal-loc-8"
+              className="animal-loc-8"
+            />
+          </div>
+        </div>
+      </main>
+
+      <footer id="footer" className="footer"></footer>
+      <script type="module" src="/src/main.tsx"></script>
+    </div>
+  );
 };
