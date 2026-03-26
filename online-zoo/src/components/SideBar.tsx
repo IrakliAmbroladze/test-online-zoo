93ebd266610f417ccc4d995c80075c17b67d7e08
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-26 18:19:22 +0400

    feat(favSideBar): finish draft version including star icon

--- online-zoo/src/components/SideBar.tsx
+++ online-zoo/src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import { Star } from "lucide-react";
 import { useState } from "react";
 
 export const SideBar = () => {
@@ -5,17 +6,21 @@ export const SideBar = () => {
   const list: string[] = ["one", "two"];
   return (
     <section className="sidebar-container">
-      <div className={`cameras ${shrinked && "shrinked"}`}>
-        <div
-          className="double_arrow"
-          onClick={() => setShrinked((prev) => !prev)}
-        >
-          <img src="/assets/icons/double_arrow.svg" alt="double_arrow" />
+      <div className={`sidebar ${shrinked && "shrinked"}`}>
+        <div className="sidebar-header">
+          <div className="title">
+            {shrinked ? <Star color="#dfe228" /> : "my favourite pets"}
+          </div>
+
+          <div
+            className="double_arrow"
+            onClick={() => setShrinked((prev) => !prev)}
+          >
+            <img src="/assets/icons/double_arrow.svg" alt="double_arrow" />
+          </div>
         </div>
-        <div className="sidebar">
-          {list.map((item) => (
-            <div className="pet-camera-card">{item}</div>
-          ))}
+        <div className="list">
+          {!shrinked && list.map((item) => <div className="item">{item}</div>)}
         </div>
       </div>
     </section>
